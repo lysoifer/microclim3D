45cabd4de9a6c5dfaab624d0657aba0c61a29394
lysoifer <lysoifer@gmail.com>
2025-06-11 15:33:08 -0400

    get gedi tiles function

--- R/downloads.R
+++ R/downloads.R
@@ -1,21 +1,28 @@
 #' Download GEDI tiles
+#'
+#'@description
+#'Download cleaned tiles of NASA GEDI L2b data. Tiles are indexed by the lower left corner of the quadrant.
+#'Each tile is 1x1 degree. Cleaning of GEDI data was done following methods in
+#'Burns et al. 2024. Multi-resolution gredded maps of vegetation structure from GEDI
+#'https://doi.org/10.1038/s41597-024-03668-4
+#'
 #' @param e spatraster, spatvector, or spatextent
-#' @param fpath file path for zip files
-#' @param unzip logical; should the downloaded file be unzipped
-#' warning if true, file sizes will be large
+#' @param fpath file path for downloaded GEDI tiles
+#' @param unzip logical; should the downloaded file be unzipped warning if true, file sizes will be large
 #' @param download logical (default TRUE) if TRUE will download gedi tiles, if false will only return file paths
 #'
+#' @return vector names of filepaths and downloads gedi tiles if download == TRUE
 #' @export
 #'
 #'
-get_gedi_tile = function(e, fpath, unzip = F, download = T) {
+get_gedi_tiles = function(e, fpath, unzip = F, download = T) {
   # download GEDI tiles from Patrick Burns' prefiltered tile footprint database
   # see filters in Burns et al. 2024
   # tiles are indexed by lower left corner of the quadrant
   # each tile is 1 by 1 degree
 
-  if(class(e) == "SpatVector" | class(e) == "SpatRaster") {
-    e = ext(e)
+  if(methods::is(e, "SpatVector") | methods::is(e, "SpatRaster")) {
+    e = terra::ext(e)
   }
 
   xmin = floor(e[1])
@@ -30,14 +37,14 @@ get_gedi_tile = function(e, fpath, unzip = F, download = T) {
     for(y in ys[1:length(ys)]) {
       url = "https://rcdata.nau.edu/geode_data/GEDIv002_L0204A_20190417to20230316_proc202312/tables/gediv002_l2l4a_va_g"
       if(x<0) {
-        lon = paste0(f_pad_zero(abs(x),3),"w")
+        lon = paste0(numform::f_pad_zero(abs(x),3),"w")
       } else {
-        lon = paste0(f_pad_zero(x, 3), "e")
+        lon = paste0(numform::f_pad_zero(x, 3), "e")
       }
       if(y<0) {
-        lat = paste0(f_pad_zero(abs(y), 2), "s")
+        lat = paste0(numform::f_pad_zero(abs(y), 2), "s")
       } else {
-        lat = paste0(f_pad_zero(y, 2), "n")
+        lat = paste0(numform::f_pad_zero(y, 2), "n")
       }
       url = paste0(url,lon,lat,".zip")
 
@@ -47,7 +54,7 @@ get_gedi_tile = function(e, fpath, unzip = F, download = T) {
 
       fname.csv = gsub("zip", "csv", fname)
       if(download & (!(file.exists(fname) | file.exists(fname.csv)))) {
-        dl = try(download.file(url = url, destfile = fname))
+        dl = try(utils::download.file(url = url, destfile = fname))
         if(dl != 0) {
           warning(paste0("Download of ", url, " failed. Tile might be in the ocean"))
         }
