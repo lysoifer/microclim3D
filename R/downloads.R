181724b2ff220514bfcbb41998c09d7ce7c87404
lysoifer <lysoifer@gmail.com>
2025-10-11 11:50:15 -0400

    bug fix get_soil2

--- R/downloads.R
+++ R/downloads.R
@@ -178,15 +178,15 @@ get_soil2 = function(x, coords = c("x", "y"), crs = "epsg:4326",
 
       # name for writing out tiles by extent
       ename = paste(c(ext(r)[1], ext(r)[2], ext(r)[3], ext(r)[4]), collapse = "_")
-      fout = paste0(fout, "soilGrids_depthWeightedSoil_", ename, ".tif")
+      fname = paste0(fout, "soilGrids_depthWeightedSoil_", ename, ".tif")
 
       # make a temporary directory to store downloads - necessary to parallelize so workers don't share tempdirs
       tempdir1= paste0(tempdir_location, i, cc)
       if(!dir.exists(tempdir1)) {dir.create(tempdir_location, recursive = T)}
 
-      if(!file.exists(fout) | overwrite) {
+      if(!file.exists(fname) | overwrite) {
         soil = microclimdata::soildata_download(r, pathdir = tempdir1, deletefiles = T)
-        writeRaster(soil, fout, overwrite)
+        writeRaster(soil, fname, overwrite)
       }
       unlink(tempdir1)
     }
